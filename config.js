e9b1e63116622a384a396d7d7f3a9339bb0cd3cf
Yousaf Jan <yousafjan123ytt@gmail.com>
2025-10-06 09:44:27 -0700

    Add connection test to config.js

--- config.js
+++ config.js
@@ -3,13 +3,14 @@
 const SUPABASE_URL = 'https://anawmgbaztruwxiaupql.supabase.co'; 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFuYXdtZ2JhenRydXd4aWF1cHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MDYwNzQsImV4cCI6MjA3NDIyMjA3NH0.631QDQmIwWfWnXXowPXS0YtQlMpX89LR-GajXkNOlzk';
 
-// Supabase library se 'createClient' function nikalein
-// Yeh function HTML mein <script> tag se aa raha hai
 const { createClient } = supabase;
-
-// Supabase se connect karne ke liye client banayein
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// Is client ko doosri files (jaise auth.js, listings.js) mein istemal karne ke liye export karein
+// --- CONNECTION TEST ---
+// Yeh line check karegi ke client theek se bana hai ya nahi.
+console.log('Supabase Client Object:', supabaseClient);
+// --------------------
+
+// Is client ko doosri files mein istemal karne ke liye export karein
 export { supabaseClient };
 
